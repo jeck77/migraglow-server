4ca62c0f71c58f0601b30e9d6924af2458cc9f08
anjeongmin <jeck77@naver.com>
2026-07-15 20:51:34 +0900

    Initial Spring Boot project

---
+++ src/main/java/io/migraflow/MigraflowServerApplication.java
@@ -0,0 +1,14 @@
+package io.migraflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MigraflowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigraflowServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/migraflow/MigraflowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.migraflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigraflowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
